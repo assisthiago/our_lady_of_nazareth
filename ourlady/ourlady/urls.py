b075f1a3ea79165a3887cab57f0b5bae511759ba
Thiago Assis <thiago.medassis@gmail.com>
2021-02-09 20:27:44 -0300

    Navbar e Carousel

--- ourlady/ourlady/urls.py
+++ ourlady/ourlady/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('pages.urls')),
+    path('', include('home.urls'), name='home'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
